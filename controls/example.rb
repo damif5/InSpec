7e712cd3c2f670f593af84b7b0db592fa27b4934
damif5 <115185898+damif5@users.noreply.github.com>
2022-11-03 10:37:15 -0400

    Test updates

--- controls/example.rb
+++ controls/example.rb
@@ -8,9 +8,9 @@ describe file("/tmp") do
 end
 
 # you add controls here
-control "tmp-1.0" do                        # A unique ID for this control
+control "tmp-2.0" do                        # A unique ID for this control
   impact 0.7                                # The criticality, if this control fails.
-  title "Create /tmp directory"             # A human-readable title
+  title "Create /tmp directory updated"             # A human-readable title
   desc "An optional description..."
   describe file("/tmp") do                  # The actual test
     it { should be_directory }
